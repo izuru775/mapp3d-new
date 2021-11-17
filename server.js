04af1e760d88c3a9f8bfb23b626fe643993d6f52
izuru775 <izuru775@gmail.com>
2021-11-17 21:11:03 +1100

    404 html send to incorrect paths

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ const app = express();
 app.use(express.static(__dirname+'/public'));
 app.use(express.urlencoded({extended:true}));
 
-
-app.get("/api",(req,res)=>{
-    res.json({message:"Hello from server"});
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'public','404.html'));
 });
-
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT,()=>{
     console.log(`Server listning on ${PORT}`)
-})
+});
