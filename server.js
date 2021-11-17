9043b77254283772caa4c6b9127cad91bd2b3ded
izuru775 <izuru775@gmail.com>
2021-11-17 20:28:26 +1100

    path set to public folder

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-
-const PORT = process.env.PORT || 3001;
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(__dirname+'/public'));
+app.use(express.urlencoded({extended:true}));
+
+
 app.get("/api",(req,res)=>{
     res.json({message:"Hello from server"});
-})
+});
 
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT,()=>{
     console.log(`Server listning on ${PORT}`)
 })
