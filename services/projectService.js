7cdee8ad064d123bc2766fa71525d5fef5a36f9e
izuru775 <izuru775@gmail.com>
2022-01-10 16:25:05 +1100

    ProjectServices created

--- services/projectService.js
+++ services/projectService.js
@@ -1,1 +1,17 @@
-const mongoose = require('mongoose');
+const VR = require('../models/3dmodel');
+
+const getDataService=(req,res)=>{
+    VR.find({})
+    .then((result) => {
+      if (result) {
+        res.json({ statusCode: 200, data: result });
+      } else {
+        res.json({ statusCode: 400, message: err });
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
+module.exports ={
+    getDataService
+}
