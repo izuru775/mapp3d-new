3c4272c9bb906ca01ff101d5c996aa44d10b70d9
izuru775 <izuru775@gmail.com>
2022-01-10 16:24:07 +1100

    Minor changes

--- routes/projects.js
+++ routes/projects.js
@@ -1,56 +1,53 @@
-const VR = require('../models/3dmodel');
-const express = require('express');
+const VR = require("../models/3dmodel");
+const express = require("express");
 const router = express.Router();
+const ProjectControllers = require("../controllers");
 
-router.get('/',(req,res)=>{
-    VR.find({}).
-        then(result=>{
-            if(result){
-                res.json({statusCode:200,data:result})
-            }else{
-                res.json({statusCode:400,message:err})
-            }
-        })
-        .catch(err=>console.log(err))
-})
-
-router.post('/',(req,res)=>{
-    let {environmentName,environmentCreator,vrObject} = req.body;
-    console.log(req.body);
-    if(req.body){
-        const newVR = new VR({
-            environmentName,
-            environmentCreator,
-            vrObject
-        })
-        newVR.save()
-        .then(result =>{
-            if(result){
-                res.json({statusCode:200,message:"object successfully added", data:result})
-            }else{
-                res.json({statusCode:400,message:err})
-            }
-        })
-        .catch(err=>console.log(err));
-    }
-    
-    
+router.get("/", (req, res) => {
+  ProjectControllers.projectService.getDataController(req,res);
+  
 });
 
-router.get('/home',(req,res)=>{
-    res.render('index',{title:"Home"})
-})
+router.post("/", (req, res) => {
+  let { environmentName, environmentCreator, vrObject } = req.body;
+  console.log(req.body);
+  if (req.body) {
+    const newVR = new VR({
+      environmentName,
+      environmentCreator,
+      vrObject,
+    });
+    newVR
+      .save()
+      .then((result) => {
+        if (result) {
+          res.json({
+            statusCode: 200,
+            message: "object successfully added",
+            data: result,
+          });
+        } else {
+          res.json({ statusCode: 400, message: err });
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+});
 
-router.get('/environments',(req,res)=>{
-    res.render('environments',{title:'Environments'})
-})
-router.get('/myEnvironments',(req,res)=>{
-    res.render('myEnvironments',{title:'My Environments'})
-})
-router.get('/miami',(req,res)=>{
-    res.render('miami',{title:'My Environments'})
-})
-router.get('/360video',(req,res)=>{
-    res.render('360video',{title:'360 video'})
-})
+router.get("/home", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+router.get("/environments", (req, res) => {
+  res.render("environments", { title: "Environments" });
+});
+router.get("/myEnvironments", (req, res) => {
+  res.render("myEnvironments", { title: "My Environments" });
+});
+router.get("/miami", (req, res) => {
+  res.render("miami", { title: "My Environments" });
+});
+router.get("/360video", (req, res) => {
+  res.render("360video", { title: "360 video" });
+});
 module.exports = router;
